038b09f65e723fe1c54628a8d4f1dd0106f002eb
jpogue0 <108087479+jpogue0@users.noreply.github.com>
2022-06-26 13:32:27 -0700

    Add files via upload

---
+++ Flow1.py
@@ -0,0 +1,7 @@
+#Simple therapeutics model v.1
+a = input("Drug name")
+print(a)
+x = int(input("Patient Population"))
+y = int(input("Drug Price"))
+number = x * y
+print("Projected Revenue", number)
